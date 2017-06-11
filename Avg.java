f5a1c1293279b8941529ff51fb0091b7be7ea8d0
GokulSGK <gokul.dinesh1997@gmail.com>
2017-06-11 13:54:35 +0530

    Create Avg.java

---
+++ Avg.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Avg {
+
+	public static void main(String[] args) {
+		int x=0;
+	    int sum1=0,sum2=0;
+	    Scanner s = new Scanner(System.in);
+	    int n = s.nextInt();
+	    int abc[] = new int[n];
+	    for(int i=0;i<n;i++)
+	    {
+	    	abc[i] = s.nextInt();
+	    }
+	    int[] first=new int[3];
+	    int[] second=new int[abc.length/2];
+	  for(int i=0;i<=abc.length/2;i++)
+	  {
+	    first[i]=abc[x];
+	    sum1=sum1+first[i];
+		x++;
+	  }
+	  System.out.println(sum1);
+	    for(int j=0;j<abc.length/2;j++)
+	  {
+	    second[j]=abc[x];
+	    sum2=sum2+second[j];
+		x++;
+	  }
+	    System.out.println(sum2);
+	    if(sum1==sum2)
+	    {
+	      for(int k=0;k<=abc.length/2;k++)
+	      {
+	        System.out.print(first[k]+" ");
+	      }
+		   System.out.print("\n");
+	      for(int l=0;l<abc.length/2;l++)
+	      {
+	        System.out.print(second[l]+" ");
+
+	}}
+	}}
+
+
